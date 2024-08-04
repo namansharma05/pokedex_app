a56d75e816237a1e496f8ab2c832ddae2317f865
Naman <namanks0511@gmail.com>
2024-08-04 18:10:18 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_app/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'PokeDex',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.lightGreen,
+        ),
+        useMaterial3: true,
+        textTheme: GoogleFonts.quattrocentoSansTextTheme(),
+      ),
+      home: HomePage(),
+    );
+  }
+}
